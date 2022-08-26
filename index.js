31f0a3de7fe545ba6bfc9035d69b0b402e01ef96
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-08-26 15:47:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Olá, T5!");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+})
